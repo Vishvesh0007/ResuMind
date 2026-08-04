3989510642e6fbfb2a2cb4411bf771dc0fdcc275
Vishvesh Singh <iamalitleboy007@gmail.com>
2026-08-04 21:24:02 +0530

    feat: add resume detail route to display parsed feedback and document previews

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,4 +1,6 @@
-import { Link, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => ([
     { title: 'Resumeind | Review' },
@@ -6,7 +8,39 @@ export const meta = () => ([
 ])
 
 const Resume = () => {
+    const { auth, isLoading, fs, kv } = usePuterStore();
     const { id } = useParams();
+    const [imageUrl, setImageUrl] = useState();
+    const [resumeUrl, setResumeUrl] = useState();
+    const [feedback, setFeedback] = useState();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const resume = await kv.get(`resume:${id}`);
+
+            if (!resume) return;
+
+            const data = JSON.parse(resume);
+
+            const resumeBlob = await fs.read(data.resumePath);
+            if (!resumeBlob) return;
+
+            const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+            const resumeUrl = URL.createObjectURL(pdfBlob);
+
+            setResumeUrl(resumeUrl);
+
+            const imageBlob = await fs.read(data.imagePath);
+            if (!imageBlob) return;
+            const imageUrl = URL.createObjectURL(imageBlob);
+            setImageUrl(imageUrl);
+
+            setFeedback(data.feedback);
+        }
+
+    }, [id]);
     return (
         <main className="!pt-0">
             <nav className="resume-nav">
@@ -16,7 +50,10 @@ const Resume = () => {
                 </Link>
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                <section className="feedback-section">
+                    {imageUrl && resumeUrl && (
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit"></div>
+                    )}
 
                 </section>
             </div>
